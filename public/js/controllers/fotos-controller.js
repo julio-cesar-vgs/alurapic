f598c18d775b3fc0317da0b7dc63781a399d16ce
Sleepk <julio.cesar.vgs@gmail.com>
2018-09-09 19:07:37 -0300

    Aula 3: Tornando nossa aplicação mais próxima de uma aplicação real, conclusao da aula

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,20 +1,43 @@
-angular.module('alurapic').controller('FotosController', function ($scope) {
-
-    $scope.fotos = [
-        {
+angular.module('alurapic').controller('FotosController', function ($scope, $http) {
+    /** carregamento manual das fotos
+     $scope.fotos = [
+     {
             titulo: 'Leão',
             url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
         },
 
-        {
+     {
             titulo: 'Leão2',
             url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
         },
 
-        {
+     {
             titulo: 'Leão3',
             url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
         }
-    ];
+     ];
 
+     // carregamento dinamico das fotos
+     $scope.fotos = [];
+
+     var promisse = $http.get('v1/fotos');
+
+     promisse.then(function (retorno) {
+        $scope.fotos = retorno.data;
+
+    }).catch(function (error) {
+        console.log(error);
+
+    })
+     */
+
+
+
+    $http.get('v1/fotos').success(function (fotos) {
+        $scope.fotos = fotos;
+
+    }).error(function (erro) {
+        console.log(erro);
+
+    })
 });
